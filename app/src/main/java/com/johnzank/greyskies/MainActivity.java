da906e9db80a0f2e7d41b71d322c9811637ffa9a
John Zank <john.k.zank@gmail.com>
2015-08-28 22:17:50 -0400

    Configured alert dialog.

--- app/src/main/java/com/johnzank/greyskies/MainActivity.java
+++ app/src/main/java/com/johnzank/greyskies/MainActivity.java
@@ -41,14 +41,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Response response) throws IOException {
                 try {
-
+                    Log.v(TAG, response.body().string());
                     if (response.isSuccessful()) {
-                        Log.v(TAG, response.body().string());
+
+                    }
+                    else {
+                        alertUserAboutError();
                     }
                 } catch (IOException e) {
                     Log.e(TAG, "Exception caught: ", e);
                 }
             }
         });
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 }
